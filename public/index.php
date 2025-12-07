296693762b6134cfa15f0d31859cc2d6b6d2c2cb
Hendrizzzz <jimhendrixbageo@gmail.com>
2025-12-08 02:50:13 +0800

    Fix RewriteBase and force display errors

--- public/index.php
+++ public/index.php
@@ -3,6 +3,9 @@
 declare(strict_types=1);
 
 session_start();
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
 // CORS Headers (Crucial for your AJAX fetch calls to work smoothly)
 header("Access-Control-Allow-Origin: *");
