cf27ffb4e8e990d34ecd6fa1e7b59d92d5e5b07f
Hendrizzzz <jimhendrixbageo@gmail.com>
2025-12-09 16:11:51 +0800

    config: saved database config

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
-// config/database.php
 
 return [
     'host' => '127.0.0.1',
-    'user' => '', # your username boi
-    'pass' => '', # your password boi
-    'name' => 'slubazaar' # name the file this way boi
-]; # AND DO NOT COMMIT ANY CHANGES HERE, WE HAVE DIFFERENT ACCOUNTS
+    'user' => 'root',
+    'pass' => '',
+    'name' => 'slubazaar',
+    'port' => 3306
+];
