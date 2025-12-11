7be3d613b517d01469fac587aba7ca89b6cc3878
Hendrizzzz <jimhendrixbageo@gmail.com>
2025-12-12 04:43:16 +0800

    feat: render needed files and update the mysql file for the added item photos

--- config/database.php
+++ config/database.php
@@ -1,8 +1,27 @@
 <?php
 
-return [
-    'host' => '127.0.0.1',
-    'user' => 'slu_admin', # your username boi
-    'pass' => 'password123', # your password boi
-    'name' => 'slubazaar' # name the file this way boi
-]; # AND DO NOT COMMIT ANY CHANGES HERE, WE HAVE DIFFERENT ACCOUNTS
+function getEnvVar($key, $default = null)
+{
+    $val = getenv($key);
+    return ($val !== false) ? $val : $default;
+}
+
+// Render config
+if (getEnvVar('DB_HOST')) {
+    return [
+        'host' => getEnvVar('DB_HOST'),
+        'user' => getEnvVar('DB_USER'),
+        'pass' => getEnvVar('DB_PASS'),
+        'name' => getEnvVar('DB_NAME'),
+        'port' => getEnvVar('DB_PORT', 3306)
+    ];
+} else {
+    // LOCAL CONFIG (XAMPP defaults)
+    return [ #DEFAULTS
+        'host' => 'localhost', #PAG NI-UPDATE MO TO, WAG MO NANG IPUSH
+        'user' => 'root', #PAG NI-UPDATE MO TO, WAG MO NANG IPUSH
+        'pass' => '',    #PAG NI-UPDATE MO TO, WAG MO NANG IPUSH
+        'name' => 'slubazaar',
+        'port' => 3306
+    ];
+}
